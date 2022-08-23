1c7ac28a6b0e28d4488b3f19801db4fef9da64a3
Mostafa1682002 <ma9856603@gmail.com>
2022-08-24 00:31:17 +0200

    Uploaded All File

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,33 @@
 let allProduct=document.querySelectorAll(".products-boxs .product")
-
-let perviewProductBox=document.querySelector(".preview-products");
-let perviewProducts=document.querySelectorAll(".preview-products .preview");
-
+let nameProduct,srcImge,price;
 
 allProduct.forEach((product)=>{
-    product.onclick=(e)=>{
-        let pNumber=product.getAttribute('data-name');
-        perviewProducts.forEach((preview)=>{
-            if(preview.getAttribute('data-name')==pNumber){
-                preview.classList.add("show")
-            }
-        })
+    product.addEventListener('click',()=>{
+        srcImge=product.querySelector('img');
+        nameProduct=product.querySelector('h3');
+        price=product.querySelector(".price");
+        //Call Function
+        changePreview(srcImge,nameProduct,price);
+
         perviewProductBox.classList.add('active');
-    }
-});
-
-
-let allClose=document.querySelectorAll(".fa-times");
-allClose.forEach((clo)=>{
-    clo.onclick=()=>{
-        perviewProducts.forEach((e)=>{
-            e.classList.remove("show")
-        })
-        perviewProductBox.classList.remove('active');
-    }
+    })
 })
+
+let perviewProductBox=document.querySelector(".preview-products");
+let perview=document.querySelector(".preview-products .preview");
+let closePreview=perview.querySelector('.fa-times');
+
+function changePreview(srcI,proNam,pri){
+    let curentImg=perview.querySelector("img");
+    curentImg.src=srcI.src;
+
+    let curentName=perview.querySelector("h3");
+    curentName.innerHTML=`Organic ${proNam.innerHTML}`; 
+
+    let curentPrice=perview.querySelector(".price");
+    curentPrice.innerHTML=pri.innerHTML;
+}
+
+closePreview.onclick=function(){
+    perviewProductBox.classList.remove('active');
+}
